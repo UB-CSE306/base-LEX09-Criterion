6c21482b78f4b4ad134788c3846e3d3ce866ff07
Carl Alphonce <alphonce@buffalo.edu>
2023-02-26 18:20:28 -0500

    Initial commit

---
+++ program.c
@@ -0,0 +1,8 @@
+/* You will need to add your definitions of these functions below:
+
+        int UPC_IsValid(char *);
+        int ISBN10_IsValid(char *);
+        int ISBN13_IsValid(char *);
+
+        int code_IsValid(char *);
+*/
---
+++ program.h
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+int UPC_IsValid(char *);
+int ISBN10_IsValid(char *);
+int ISBN13_IsValid(char *);
+
+int code_IsValid(char *);
